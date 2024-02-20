30bf746dde7a678b64a0aa4d75e084d8a9baa12b
Sakib-Atreus <sakibatreus@gmail.com>
2024-02-21 03:12:05 +0600

    Dashboard Customize

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 import useCart from "../../../hooks/useCart";
 import { FaTrashAlt } from "react-icons/fa";
 import Swal from "sweetalert2";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const MyCart = () => {
   const [cart, refetch] = useCart();
@@ -45,52 +46,60 @@ const MyCart = () => {
       <Helmet>
         <title>Bistro Delicioso | My Cart</title>
       </Helmet>
-      <div className="uppercase font-bold h-[60px] flex justify-evenly items-center">
-        <h3 className="text-2xl ">Total Items: {cart.length}</h3>
-        <h3 className="text-2xl ">Total Price: ${total}</h3>
-        <button className="btn bg-[#D1A054] btn-sm text-white ">PAY</button>
-      </div>
-      <div className="overflow-x-auto">
-        <table className="table">
-          {/* head */}
-          <thead className="bg-base-200">
-            <tr>
-              <th>#</th>
-              <th>Food</th>
-              <th>Item Name</th>
-              <th>Price</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cart.map((item, index) => (
-              <tr key={item._id}>
-                <td>{index + 1}</td>
-                <td>
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src={item.image}
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                </td>
-                <td>{item.name}</td>
-                <td className="text-end">${item.price}</td>
-                <td>
-                  <button
-                    onClick={() => handleDelete(item)}
-                    className="btn btn-ghost bg-red-600 text-white"
-                  >
-                    <FaTrashAlt></FaTrashAlt>
-                  </button>
-                </td>
+      <SectionTitle
+        subHeading={"My Cart"}
+        heading={"WANNA ADD MORE?"}
+      ></SectionTitle>
+      <section className="bg-base-200">
+        <div className="uppercase font-bold  my-4 p-2 h-[60px] flex justify-evenly items-center">
+          <h3 className="text-2xl ">Total Items: {cart.length}</h3>
+          <h3 className="text-2xl ">Total Price: ${total}</h3>
+          <button className="btn bg-[#D1A054] btn-sm text-black font-bold ">
+            PAY
+          </button>
+        </div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead className="bg-base-200">
+              <tr className="bg-[#D1A054]">
+                <th>#</th>
+                <th>Food</th>
+                <th>Item Name</th>
+                <th>Price</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {cart.map((item, index) => (
+                <tr key={item._id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </td>
+                  <td>{item.name}</td>
+                  <td className="text-end">${item.price}</td>
+                  <td>
+                    <button
+                      onClick={() => handleDelete(item)}
+                      className="btn btn-ghost bg-red-600 text-white"
+                    >
+                      <FaTrashAlt></FaTrashAlt>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
     </div>
   );
 };
